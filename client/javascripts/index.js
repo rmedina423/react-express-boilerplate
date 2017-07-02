e88b91923ddc761d035e0c12ca17b06db24e08ff
Ryan M. Medina <rmedina423@gmail.com>
2017-07-02 12:41:52 -0700

    add missing style import

--- client/javascripts/index.js
+++ client/javascripts/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from './Router';
+import '../stylesheets/styles.scss';
 
 ReactDOM.render(<Router />, document.getElementById('app-container'));
